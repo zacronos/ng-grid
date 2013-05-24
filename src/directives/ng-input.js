fd5c0f65c4bcf7bc358695891e88c5800bca89a6
Brian Hann <emailc0bra@gmail.com>
2013-05-24 09:47:30 -0500

    Jshint: cleaning up ng-input.js

--- src/directives/ng-input.js
+++ src/directives/ng-input.js
@@ -6,48 +6,49 @@
 
     var evaled = $scope.$eval(inputter);
     var getter = $parse(evaled);
-		var setter = getter.assign;
-		var oldCellValue = getter($scope);
+    var setter = getter.assign;
+    var oldCellValue = getter($scope);
 
-		elm.val(oldCellValue);
+    elm.val(oldCellValue);
 
     elm.bind('keyup', function() {
         var newVal = elm.val();
         // dump(newVal);
         if (!$scope.$root.$$phase) {
-          $scope.$apply(function(){
-            setter($scope, newVal);
-          });
+            $scope.$apply(function() {
+                setter($scope, newVal);
+            });
         }
     });
 
-		elm.bind('keydown', function(evt){
-			switch(evt.keyCode){
-				case 37: // Left arrow
-				case 38: // Up arrow
-				case 39: // Right arrow
-				case 40: // Down arrow
-					evt.stopPropagation();
-					break;
-				case 27: // Esc (reset to old value)
-					if (!$scope.$root.$$phase) {
-						$scope.$apply(function(){
-							setter($scope, oldCellValue);
-							elm.val(oldCellValue);
-							elm.blur();
-						});
-					}
-        case 13: // Enter (apply new value)
-          if (!$scope.$root.$$phase) {
-            $scope.$apply(function(){
-              setter($scope, elm.val());
-              elm.blur();
-            });
-          }
-				default:
-					break;
-			}
-			return true;
-		});
+    elm.bind('keydown', function(evt) {
+        switch(evt.keyCode) {
+            case 37: // Left arrow
+            case 38: // Up arrow
+            case 39: // Right arrow
+            case 40: // Down arrow
+                evt.stopPropagation();
+                break;
+            case 27: // Esc (reset to old value)
+                if (!$scope.$root.$$phase) {
+                    $scope.$apply(function(){
+                        setter($scope, oldCellValue);
+                        elm.val(oldCellValue);
+                        elm.blur();
+                    });
+                }
+                break;
+            case 13: // Enter (apply new value)
+                if (!$scope.$root.$$phase) {
+                    $scope.$apply(function(){
+                        setter($scope, elm.val());
+                        elm.blur();
+                    });
+                }
+                break;
+        }
+
+        return true;
+    });
   };
 }]);
